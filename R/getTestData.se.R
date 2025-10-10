1f5a16fd5ceda2ec3bdfdeef591a213996bfbf76
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-10-10 11:28:56 -0700

    Added wrappers for the CRISPR QC functions.

--- R/getTestData.se.R
+++ R/getTestData.se.R
@@ -1,5 +1,7 @@
 cache <- new.env()
 cache$rna <- list()
+cache$adt <- list()
+cache$crispr <- list()
 
 #' Get a test scRNA-seq dataset
 #'
@@ -10,21 +12,27 @@ cache$rna <- list()
 #'
 #' @return A \link[SingleCellExperiment]{SingleCellExperiment} containing a dataset at the specified level of processing.
 #' 
+#' @details
 #' For \code{getTestRnaData}, this is a scRNA-seq dataset of the mouse brain,
 #' where the main experiment contains RNA counts and the alternative experiments contain ERCC and repeat element counts.
+#' This is obtained with \code{fetchDataset("zeisel-brain-2015", "2023-12-14")}.
 #'
 #' For \code{getTestAdtData}, this is a CITE-seq dataset of human PBMCs,
 #' where the main experiment contains RNA counts and the alternative experiment contains ADT counts.
+#' This is obtained with \code{fetchDataset("kotliarov-pbmc-2020", "2024-04-18")}.
+#'
+#' For \code{getTestCrisprData}, this is a Perturb-seq dataset of a pancreatic beta cell line,
+#' where the main experiment contains RNA counts and the alternative experiment contains CRISPR guide counts.
+#' This is obtained with \code{fetchDataset("cao-pancreas-2025", "2025-10-10", "rqc")}.
 #'
 #' @author Aaron Lun
 #' @examples
 #' getTestRnaData.se()
 #' getTestAdtData.se()
+#' getTestCrisprData.se()
 #'
 #' @seealso
-#' \code{\link[scRNAseq]{ZeiselBrainData}}, for the original data source for \code{getTestRnaData.se}.
-#'
-#' \code{\link[scRNAseq]{KotliarovPBMCData}}, for the original data source for \code{getTestAdtData.se}.
+#' \code{\link[scRNAseq]{fetchDataset}}, used to obtain each dataset.
 #' 
 #' @export
 #' @name getTestData.se
@@ -32,7 +40,7 @@ getTestRnaData.se <- function(at = "start") {
     at <- match.arg(at)
 
     if (!("start" %in% names(cache$rna))) {
-        cache$rna$start <- scRNAseq::ZeiselBrainData()
+        cache$rna$start <- scRNAseq::fetchDataset("zeisel-brain-2015", "2023-12-14", realize.assays=TRUE)
     }
     if (at == "start") {
         return(cache$rna[[at]])
@@ -45,9 +53,22 @@ getTestAdtData.se <- function(at = "start") {
     at <- match.arg(at)
 
     if (!("start" %in% names(cache$adt))) {
-        cache$adt$start <- scRNAseq::KotliarovPBMCData()
+        cache$adt$start <- scRNAseq::fetchDataset("kotliarov-pbmc-2020", "2024-04-18", realize.assays=TRUE)
     }
     if (at == "start") {
         return(cache$adt[[at]])
+    }
+}
+
+#' @export
+#' @rdname getTestData.se
+getTestCrisprData.se <- function(at = "start") {
+    at <- match.arg(at)
+
+    if (!("start" %in% names(cache$crispr))) {
+        cache$crispr$start <- scRNAseq::fetchDataset("cao-pancreas-2025", "2025-10-10", "rqc", realize.assays=TRUE)
+    }
+    if (at == "start") {
+        return(cache$crispr[[at]])
     }
 }
---
+++ R/quickCrisprQc.se.R
@@ -0,0 +1,90 @@
+#' Quick quality control for CRISPR data
+#'
+#' Quickly compute quality control (QC) metrics, thresholds and filters from CRISPR data in a \link[SummarizedExperiment]{SummarizedExperiment}.
+#' 
+#' @param x A \link[SummarizedExperiment]{SummarizedExperiment} object or one of its subclasses.
+#' Rows correspond to CRISPR guides and columns correspond to cells.
+#' @inheritParams scrapper::computeCrisprQcMetrics
+#' @inheritParams scrapper::suggestCrisprQcThresholds
+#' @param assay.type Integer or string specifying the assay of \code{x} containing the CRISPR count matrix.
+#' @param prefix String containing a prefix to append to the name of each column corresponding to a QC metric in the \code{link[SummarizedExperiment]{colData}}.
+#' @param flatten Logical scalar indicating whether to flatten the subset proportions into separate columns of the \code{link[SummarizedExperiment]{colData}}.
+#' If \code{FALSE}, the subset proportions are stored in a nested \link[S4Vectors]{DataFrame}.
+#' @param raw Logical scalar indicating whether to return the QC results directly.
+#' 
+#' @return
+#' For \code{quickCrisprQc.se}:
+#' \itemize{
+#' \item If \code{raw=FALSE}, \code{x} is returned with additional columns added to its \code{\link[SummarizedExperiment]{colData}}.
+#' Each column contains per-cell values for one of the QC metrics, see \code{\link[scrapper]{computeCrisprQcMetrics}} for details.
+#' The suggested thresholds are stored as a list in \code{\link[S4Vectors]{metadata}}.
+#' The \code{colData} also contains a \code{keep} column, specifying which cells are to be retained.
+#' \item If \code{raw=TRUE}, a list is returned containing \code{metrics}, the result of \code{\link[scrapper]{computeCrisprQcMetrics}};
+#' \code{thresholds}, the result of \code{\link[scrapper]{suggestCrisprQcThresholds}},
+#' and \code{keep}, the result of \code{\link[scrapper]{FilterCrisprQcMetrics}}.
+#' }
+#'
+#' For \code{attachCrisprQcMetrics.se}. \code{x} is returned with additional columns added to its \code{colData}.
+#'
+#' @author Aaron Lun
+#'
+#' @seealso
+#' \code{\link[scrapper]{computeCrisprQcMetrics}},
+#' \code{\link[scrapper]{suggestCrisprQcThresholds}},
+#' and \code{\link[scrapper]{filterCrisprQcMetrics}}
+#' from the \pkg{scrapper} package.
+#'
+#' @examples
+#' sce <- altExp(getTestCrisprData.se(), "CRISPR Guide Capture")
+#' sce <- quickCrisprQc.se(sce)
+#' colData(sce)[,c("sum", "detected", "max.value", "max.index")]
+#' metadata(sce)$thresholds
+#' summary(sce$keep)
+#'
+#' # We can also manually execute many of the steps:
+#' sce <- altExp(getTestCrisprData.se(), "CRISPR Guide Capture")
+#' res <- quickCrisprQc.se(sce, raw=TRUE)
+#' sce <- attachCrisprQcMetrics.se(sce, res$metrics, prefix="crispr.qc.")
+#' res$thresholds$max.value <- 50 # manual override of the max value threshold
+#' sce$crispr.qc.keep <- scrapper::filterCrisprQcMetrics(res$thresholds, res$metrics)
+#' 
+#' @export
+#' @importFrom SummarizedExperiment assay colData<-
+#' @importFrom SingleCellExperiment altExp
+#' @importFrom S4Vectors metadata metadata<-
+quickCrisprQc.se <- function( 
+    x,
+    block = NULL,
+    num.mads = 3,
+    num.threads = 1,
+    assay.type = "counts",
+    prefix = NULL, 
+    flatten = TRUE,
+    raw = FALSE
+) {
+    metrics <- scrapper::computeCrisprQcMetrics(assay(x, assay.type, withDimnames=FALSE), num.threads=num.threads)
+    thresholds <- scrapper::suggestCrisprQcThresholds(metrics, block=block, num.mads=num.mads)
+    keep <- scrapper::filterCrisprQcMetrics(thresholds, metrics, block=block)
+
+    if (raw) {
+        return(list(metrics=metrics, thresholds=thresholds, keep=keep))
+    }
+
+    x <- attachCrisprQcMetrics.se(x, metrics, prefix=prefix, flatten=flatten)
+    colData(x)[[paste0(prefix, "keep")]] <- keep
+    metadata(x)[[paste0(prefix, "thresholds")]] <- thresholds
+    x
+}
+
+#' @export
+#' @rdname quickCrisprQc.se
+#' @importFrom SummarizedExperiment colData colData<-
+attachCrisprQcMetrics.se <- function(x, metrics, prefix = "", flatten = TRUE) {
+    cd <- colData(x)
+    cd[[paste0(prefix, "sum")]] <- metrics$sum
+    cd[[paste0(prefix, "detected")]] <- metrics$detected
+    cd[[paste0(prefix, "max.value")]] <- metrics$max.value
+    cd[[paste0(prefix, "max.index")]] <- metrics$max.index
+    colData(x) <- cd
+    x
+}
